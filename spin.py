329a0ee9be19ce0e46ae15656e56c2cc99114fd8
JJ <jvirg4@gmail.com>
2021-04-21 11:43:59 -0500

    Outlined the spin module for real

--- spin.py
+++ spin.py
@@ -5,11 +5,29 @@ class Box(PhotoImage):
 	def __init__(self, img:str, value:int):
                 
 		pass
+# a prize box is
+# - an int value
+# - an image 
 
+# a prize box has
+# - a fn that adds its value to the players bank
+class PrizeBox(PhotoImage):
+        def __init__(self):
+                pass
+
+# a whammy is
+# - a prize box with specific conditions
+# - an int value of -1
+# - cordinates
+# - an image of a whammy
+
+# a whammy has
+# - a value that distiguishes it from any other box
+# - maybe a function that clears a players bank and adds a whammy to their whammy count
 class Whammy(Box):
 	def __init__(self):
                 super().__init__(parent)
-                self.whammy = whammy
+                self.cords
 
         @property
         def whammy(self):
