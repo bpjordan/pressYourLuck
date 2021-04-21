332666682b493c1b3bf7bb4b1a378d5035ef2318
JJ <jvirg4@gmail.com>
2021-04-21 11:40:42 -0500

    Images are now all pngs excluding the improvements. I also conceptually outlined the spin module.

--- spin.py
+++ spin.py
@@ -1,14 +1,30 @@
 from tkinter import *
+from random import *
 
 class Box(PhotoImage):
 	def __init__(self, img:str, value:int):
+                
 		pass
 
 class Whammy(Box):
 	def __init__(self):
+                super().__init__(parent)
+                self.whammy = whammy
+
+        @property
+        def whammy(self):
+                return self._whammy
+        @whammy.setter
+        def whammy(self, rand):
+                self._whammy = PhotoImage(file="Whammy{}.png".format(randint(1,10))
                 
-		pass
 
 class SpinGame:
 	def __init__(self):
 		pass
+
+########
+# MAIN #
+########
+
+PhotoImage(file="{}.png")
